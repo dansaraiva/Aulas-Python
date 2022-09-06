9677ec31f769996bcce5c109566a473378922509
Daniel Saraiva <saraivadaniel@protonmail.com>
2022-09-06 13:19:16 -0300

    Aula Tipos de dados e Operadores

---
+++ OlaMundo.py
@@ -0,0 +1,16 @@
+
+idade = 15
+salario = 50.35
+altura = 1.59
+genero = "F"
+nome = "Rozy"
+
+print(f"IDADE = {idade}")
+print(f"SALARIO = {salario:.2f}")
+print(f"ALTURA = {altura:.2f}")
+print(f"GENERO = {genero}")
+print(f"NOME = {nome}")
+
+
+
+
---
+++ Operadores.py
@@ -0,0 +1,4 @@
+y = 5
+x = 2
+z = y * x
+print(z)
